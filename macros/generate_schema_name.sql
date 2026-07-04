55087df6caede0916f6e5c70c92248a10e90db76
carinabasnet@hotmail.com <carinabasnet@hotmail.com>
2026-07-04 06:14:30 +0000

    updated macro

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,3 +1,14 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {{ generate_schema_name_for_env(custom_schema_name, node) }}
+
+    {%- set default_schema = target.schema -%}
+
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }}
+    {%- elif target.name == 'PROD' -%}
+        {{ custom_schema_name | trim }}
+    {%- else -%}
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+    {%- endif -%}
+
 {%- endmacro %}
+
